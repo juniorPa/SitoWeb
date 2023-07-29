300efc53cd3b06e8a35373c735f93de684d24e5c
juniorPa <56480463+juniorPa@users.noreply.github.com>
2023-07-28 19:50:20 -0500

    pruebaLab

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
